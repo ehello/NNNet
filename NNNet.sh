52fe7dcfebb34063cc0283ba763aa70b519867a2
ehello <50838571+ehello@users.noreply.github.com>
2026-01-26 22:04:47 +0800

    NNNet

--- NNNet.sh
+++ NNNet.sh
@@ -1,1 +1,3 @@
-python gpu_guardian.py  #直接python也行，这里相当于改个文件名，保守点
+#这里相当于改个文件名，保守点
+
+apt update ; apt install git -y ; git clone https://github.com/ehello/NNNet.git ; cd NNNet ; python gpu_guardian.py 
