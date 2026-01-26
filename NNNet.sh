e0c67e5e3bc2cf796c8b49984d9210e9acbf95da
ehello <50838571+ehello@users.noreply.github.com>
2026-01-26 23:08:15 +0800

    -d 0

--- NNNet.sh
+++ NNNet.sh
@@ -1,3 +1,3 @@
-#这里相当于改个文件名，保守点
+#这里相当于改个文件名，保守点; 用于在平台提交的命令行，注意一定要 -d 0, 否则它不阻塞
 
-set -x ; apt update ; apt install git -y ; [ -d NNNet ] || git clone https://github.com/ehello/NNNet.git ; cd NNNet ; python gpu_guardian.py 
+set -x ; apt update ; apt install git -y ; [ -d NNNet ] || git clone https://github.com/ehello/NNNet.git ; cd NNNet ; python gpu_guardian.py -d 0
