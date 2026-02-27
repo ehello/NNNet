23255da74deb0c2ea9637dc00989ba6691177d07
wwq <wwq@qq.com>
2026-02-27 17:06:01 +0800

    1

--- NNNet.sh
+++ NNNet.sh
@@ -1,2 +1,2 @@
 #这里相当于改个文件名，保守点; 用于在平台提交的命令行
-set -x ; apt update ; apt install git -y ; [ -d NNNet ] || git clone https://github.com/ehello/NNNet.git ; cd NNNet ; python python scramble4gpu.py -n "$(nvidia-smi -L |wc -l)" -t 180000000  ; sleep inf
+set -x ; apt update ; apt install git -y ; [ -d NNNet ] || git clone https://github.com/ehello/NNNet.git ; cd NNNet ; python scramble4gpu.py -n "$(nvidia-smi -L |wc -l)" -t 180000000  ; sleep inf
